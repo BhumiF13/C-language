ac3f8c5a86bec8d2804dec328c0013a89678ef76
BhumiF13 <158535699+BhumiF13@users.noreply.github.com>
2024-02-21 09:15:19 +0530

    document6.c

--- document6.c
+++ document6.c
@@ -1,27 +1,37 @@
-//To input a string and remove extra wide spaces
+//frequency of all alphabets cointained in a string 
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-	int i=0,c=0,l,t,j;
-	char s[20];
-	printf("Input string\n");
-    gets(s);
-	l=strlen(s);
-	while(i<=l)
-	{
-		if(s[i]==' ')
-		{
-			j=i;
-		    while(j<=l)
-			{
-				s[j]=s[j+1];
-				j++;
-			}
-			l--;
-		}
-		i++;
-	}
-	printf("String is %s",s);
-	return 0;
+    int i,j,arr[52]={0};
+    char str[40];
+    puts("enter string");
+    fgets(str,40,stdin);
+    for(i=0;i<40;i++)
+    {
+        if(str[i]>=97&&str[i]<=122)
+        {
+            int res=str[i];
+            arr[res-97]=arr[res-97]+1;
+
+        }
+        if(str[i]>=65&&str[i]<=90)
+        {
+            int res=str[i];
+            arr[res+26-65]=arr[res+26-65]+1;
+
+            
+        }
+    }
+    for(i=0;i<52;i++)
+    {
+        if(arr[i]>0)
+        {
+            if(i<=25)
+            printf("%c is present %d times\n",97+i,arr[i]);
+            else
+            printf("%c is present %d times\n",39+i,arr[i]);
+        }   
+    }
+    return 0;
 }
